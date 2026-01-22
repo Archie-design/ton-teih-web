68e3accd6a88e9223c2094d0c0114997f0a93a64
Archie-design <you258654@gmail.com>
2026-01-22 20:51:17 +0800

    update 統編字體

--- app/page.tsx
+++ app/page.tsx
@@ -602,8 +602,8 @@ export default function App() {
 
       <footer className="bg-black py-12 text-center border-t border-white/5">
         <div className="max-w-7xl mx-auto px-4">
-          <p className="text-gray-500 text-[10px] font-black uppercase tracking-[0.4em] mb-4">© 2026 東鐵工程有限公司 TON TEIH. ALL RIGHTS RESERVED.</p>
-          <div className="text-gray-700 text-[10px] font-bold space-x-4">
+          <p className="text-gray-500 text-[14px] font-black uppercase tracking-[0.4em] mb-4">© 2026 東鐵工程有限公司 TON TEIH. ALL RIGHTS RESERVED. </p>
+          <div className="text-gray-400 text-[16px] font-bold space-x-4">
             <span>統一編號: 66450110</span>
           </div>
         </div>
